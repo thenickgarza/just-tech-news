36b9d11947122853c0a31852656c0c6dee1aee9c
Nick Garza <thenickgarza14@gmail.com>
2022-02-10 11:58:57 -0600

    added handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-
+const homeRoutes = require('./home-routes');
 
 
 router.use('/api', apiRoutes);
-
+router.use('/', homeRoutes);
+// Must be at the botton (LEARNED THE HARD WAY)
 router.use((req, res) => {
     res.status(404).end();
 });
